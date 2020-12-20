13930ffd08d9350ec36e5ecf63f2af74bc3a5c80
johannfazakas <johannfazakas@gmail.com>
2020-12-20 09:49:41 +0200

    explicitly listed allowed methods for cors

--- src/main/kotlin/ro/decision/maker/api/WebConfig.kt
+++ src/main/kotlin/ro/decision/maker/api/WebConfig.kt
@@ -1,6 +1,11 @@
 package ro.decision.maker.api
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod.DELETE
+import org.springframework.http.HttpMethod.GET
+import org.springframework.http.HttpMethod.PATCH
+import org.springframework.http.HttpMethod.POST
+import org.springframework.http.HttpMethod.PUT
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -11,6 +16,15 @@ class WebConfig : WebMvcConfigurer {
 
   override fun addCorsMappings(registry: CorsRegistry) {
     super.addCorsMappings(registry)
-    registry.addMapping("/**").allowedOrigins("*")
+    registry
+      .addMapping("/**")
+      .allowedOrigins("*")
+      .allowedMethods(
+        GET.name,
+        POST.name,
+        PUT.name,
+        PATCH.name,
+        DELETE.name
+      )
   }
 }
